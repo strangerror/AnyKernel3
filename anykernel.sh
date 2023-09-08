cb65b104289c0142fce10f1d32004db2b640be18
strangerror <efremenko.denis@gmail.com>
2023-09-08 14:43:34 +0300

    Changed kernel information to RedSin

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Lightning Kernel by Siimsek | Telegram @SKProject
+kernel.string=RedSin Kernel by garry.rogov
 do.devicecheck=1
 do.modules=0
 do.systemless=1
